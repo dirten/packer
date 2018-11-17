54fbb56f90976aac7788e79205e41c53a255d2d5
Megan Marsh <megan@hashicorp.com>
2018-11-16 16:29:03 -0800

    add service accounts to google-import post-processor. I suspect more work can be done to deduplicate and extend code.

--- post-processor/googlecompute-import/post-processor.go
+++ post-processor/googlecompute-import/post-processor.go
@@ -19,24 +19,27 @@ import (
 	"github.com/hashicorp/packer/template/interpolate"
 
 	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
 	"golang.org/x/oauth2/jwt"
 )
 
 type Config struct {
 	common.PackerConfig `mapstructure:",squash"`
 
-	Bucket            string            `mapstructure:"bucket"`
-	GCSObjectName     string            `mapstructure:"gcs_object_name"`
-	ImageDescription  string            `mapstructure:"image_description"`
-	ImageFamily       string            `mapstructure:"image_family"`
-	ImageLabels       map[string]string `mapstructure:"image_labels"`
-	ImageName         string            `mapstructure:"image_name"`
-	ProjectId         string            `mapstructure:"project_id"`
-	AccountFile       string            `mapstructure:"account_file"`
-	KeepOriginalImage bool              `mapstructure:"keep_input_artifact"`
-	SkipClean         bool              `mapstructure:"skip_clean"`
+	Bucket              string            `mapstructure:"bucket"`
+	GCSObjectName       string            `mapstructure:"gcs_object_name"`
+	ImageDescription    string            `mapstructure:"image_description"`
+	ImageFamily         string            `mapstructure:"image_family"`
+	ImageLabels         map[string]string `mapstructure:"image_labels"`
+	ImageName           string            `mapstructure:"image_name"`
+	ProjectId           string            `mapstructure:"project_id"`
+	AccountFile         string            `mapstructure:"account_file"`
+	KeepOriginalImage   bool              `mapstructure:"keep_input_artifact"`
+	ServiceAccountEmail string            `mapstructure:"service_account_email"`
+	SkipClean           bool              `mapstructure:"skip_clean"`
 
-	ctx interpolate.Context
+	ctx     interpolate.Context
+	Account googlecompute.AccountFile
 }
 
 type PostProcessor struct {
@@ -106,18 +109,18 @@ func (p *PostProcessor) PostProcess(ui packer.Ui, artifact packer.Artifact) (pac
 		return nil, false, fmt.Errorf("Error rendering gcs_object_name template: %s", err)
 	}
 
-	rawImageGcsPath, err := UploadToBucket(p.config.AccountFile, ui, artifact, p.config.Bucket, p.config.GCSObjectName)
+	rawImageGcsPath, err := p.UploadToBucket(ui, artifact, p.config.Bucket, p.config.GCSObjectName)
 	if err != nil {
 		return nil, p.config.KeepOriginalImage, err
 	}
 
-	gceImageArtifact, err := CreateGceImage(p.config.AccountFile, ui, p.config.ProjectId, rawImageGcsPath, p.config.ImageName, p.config.ImageDescription, p.config.ImageFamily, p.config.ImageLabels)
+	gceImageArtifact, err := p.CreateGceImage(ui, p.config.ProjectId, rawImageGcsPath, p.config.ImageName, p.config.ImageDescription, p.config.ImageFamily, p.config.ImageLabels)
 	if err != nil {
 		return nil, p.config.KeepOriginalImage, err
 	}
 
 	if !p.config.SkipClean {
-		err = DeleteFromBucket(p.config.AccountFile, ui, p.config.Bucket, p.config.GCSObjectName)
+		err = p.DeleteFromBucket(ui, p.config.Bucket, p.config.GCSObjectName)
 		if err != nil {
 			return nil, p.config.KeepOriginalImage, err
 		}
@@ -126,9 +129,13 @@ func (p *PostProcessor) PostProcess(ui packer.Ui, artifact packer.Artifact) (pac
 	return gceImageArtifact, p.config.KeepOriginalImage, nil
 }
 
-func UploadToBucket(accountFile string, ui packer.Ui, artifact packer.Artifact, bucket string, gcsObjectName string) (string, error) {
-	var driverScopes = []string{"https://www.googleapis.com/auth/devstorage.full_control"}
-	client = getClient(driverScopes)
+func (p *PostProcessor) UploadToBucket(ui packer.Ui, artifact packer.Artifact, bucket string, gcsObjectName string) (string, error) {
+	driverScopes := []string{"https://www.googleapis.com/auth/devstorage.full_control"}
+	client, err := p.getClient(driverScopes)
+	if err != nil {
+		return "", err
+	}
+
 	service, err := storage.New(client)
 	if err != nil {
 		return "", err
@@ -164,9 +171,12 @@ func UploadToBucket(accountFile string, ui packer.Ui, artifact packer.Artifact, 
 	return "https://storage.googleapis.com/" + bucket + "/" + gcsObjectName, nil
 }
 
-func CreateGceImage(accountFile string, ui packer.Ui, project string, rawImageURL string, imageName string, imageDescription string, imageFamily string, imageLabels map[string]string) (packer.Artifact, error) {
-	var driverScopes = []string{"https://www.googleapis.com/auth/compute", "https://www.googleapis.com/auth/devstorage.full_control"}
-	client = getClient(driverScopes)
+func (p *PostProcessor) CreateGceImage(ui packer.Ui, project string, rawImageURL string, imageName string, imageDescription string, imageFamily string, imageLabels map[string]string) (packer.Artifact, error) {
+	driverScopes := []string{"https://www.googleapis.com/auth/compute", "https://www.googleapis.com/auth/devstorage.full_control"}
+	client, err := p.getClient(driverScopes)
+	if err != nil {
+		return nil, err
+	}
 	service, err := compute.New(client)
 	if err != nil {
 		return nil, err
@@ -211,12 +221,16 @@ func CreateGceImage(accountFile string, ui packer.Ui, project string, rawImageUR
 	return &Artifact{paths: []string{op.TargetLink}}, nil
 }
 
-func DeleteFromBucket(accountFile string, ui packer.Ui, bucket string, gcsObjectName string) error {
-	var driverScopes = []string{"https://www.googleapis.com/auth/devstorage.full_control"}
-	client = getClient(driverScopes)
-	service, err := compute.New(client)
+func (p *PostProcessor) DeleteFromBucket(ui packer.Ui, bucket string, gcsObjectName string) error {
+	driverScopes := []string{"https://www.googleapis.com/auth/devstorage.full_control"}
+	client, err := p.getClient(driverScopes)
 	if err != nil {
-		return nil, err
+		return nil
+	}
+
+	service, err := storage.New(client)
+	if err != nil {
+		return nil
 	}
 
 	ui.Say(fmt.Sprintf("Deleting import source from GCS %s/%s...", bucket, gcsObjectName))
@@ -229,20 +243,40 @@ func DeleteFromBucket(accountFile string, ui packer.Ui, bucket string, gcsObject
 	return nil
 }
 
-func getClient(driverScopes []string) *http.Client {
+func (p *PostProcessor) getClient(driverScopes []string) (*http.Client, error) {
 	var client *http.Client
-	var account googlecompute.AccountFile
+	if p.config.Account.PrivateKey != "" {
+		// Auth with AccountFile first if provided
+		conf := jwt.Config{
+			Email:      p.config.Account.ClientEmail,
+			PrivateKey: []byte(p.config.Account.PrivateKey),
+			Scopes:     driverScopes,
+			TokenURL:   "https://accounts.google.com/o/oauth2/token",
+		}
 
-	err := googlecompute.ProcessAccountFile(&account, accountFile)
-	if err != nil {
-		return "", err
+		// Initiate an http.Client. The following GET request will be
+		// authorized and authenticated on the behalf of
+		// your service account.
+		client = conf.Client(oauth2.NoContext)
+		return client, nil
+	} else {
+		// The DefaultClient uses the DefaultTokenSource of the google lib.
+		// The DefaultTokenSource uses the "Application Default Credentials"
+		// It looks for credentials in the following places, preferring the first location found:
+		// 1. A JSON file whose path is specified by the
+		//    GOOGLE_APPLICATION_CREDENTIALS environment variable.
+		// 2. A JSON file in a location known to the gcloud command-line tool.
+		//    On Windows, this is %APPDATA%/gcloud/application_default_credentials.json.
+		//    On other systems, $HOME/.config/gcloud/application_default_credentials.json.
+		// 3. On Google App Engine it uses the appengine.AccessToken function.
+		// 4. On Google Compute Engine and Google App Engine Managed VMs, it fetches
+		//    credentials from the metadata server.
+		//    (In this final case any provided scopes are ignored.)
+		client, err := google.DefaultClient(oauth2.NoContext, driverScopes...)
+		if err != nil {
+			return nil, err
+		}
+		return client, nil
 	}
-	conf := jwt.Config{
-		Email:      account.ClientEmail,
-		PrivateKey: []byte(account.PrivateKey),
-		Scopes:     DriverScopes,
-		TokenURL:   "https://accounts.google.com/o/oauth2/token",
-	}
-	client = conf.Client(oauth2.NoContext)
-	return client
+	return nil, fmt.Errorf("Something went wrong getting the client for the GCE import post-processor")
 }
